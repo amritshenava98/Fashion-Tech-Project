8b42fc4401b87db12e0bfbd6a6f9199fbe47fc2b
Amrit Shenava <amritshenava98@users.noreply.github.com>
2019-01-26 00:09:00 -0500

    Update futuro.c

--- futuro.c
+++ futuro.c
@@ -7,27 +7,49 @@ DATE : 25/01/2019
 Project code for temperature altering wearable
 */
 
+//Including the Esplora library code.
+#include <Esplora.h>
+
 //Setup of the board
 void setup(){
-
+  
+ //Setting up the LED of the board.
+ pinMode(LED_BUILTIN, OUTPUT)
 }
 
 
 // The code after loop consists of the working part of the project.
 void loop(){
  
- //Measuring the temperature in Celsius
- int celsius = Esplora.readTemperature(DEGREES_C)
- 
  //Measuring the temperature in Farenheit
  int farenheit = Esplora.readTemperature(DEGREES_F)
  
- if(farenheit == 72)
+ // 
+ 
+  //Array of the colours
+  //colours = ["Red", "Green", "Blue"]
+  
+ //72F is a standard room temperature. If True, no action required. 
+ if(farenheit >= 72)
  {
-  print("The Temperature is normal")
+   //The Temperature is normal and will not alter
+   print("The Temperature is normal")
+   
  }
+ 
+ //The statement to be executed if the temperature is below 72F
  else if(farenheit < 72){
-  print("The Temperature is decreasing 
+   //The Temperature is decreasing and will alter
+   print("The Temperature is decreasing")
+   
+   //The Loop is to increment the temperature
+   for(int i = 0 ; i < 72 ; i++)
+   {
+    int currenttemp;
+    i = currenttemp;
+    currenttemp = farenheit;
+    currenttemp++;
+   }
  }
 
 }
